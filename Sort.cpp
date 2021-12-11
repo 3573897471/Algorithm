c12bd24f20901e2e76e8d95dc7ffa3e0ae2b6d79
Levi <3573897471@qq.com>
2021-12-11 19:22:37 +0800

    Mac 21/12/11 Sort

---
+++ Sort.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 黎炜堂 on 2021/12/11.
+//
+
+#include <iostream>
+using namespace std;
+
+
+// 交换函数
+template<class T>
+void Swap (T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+//选择排序
+void SelectSort(int *array, int len) {
+
+}
+
+
+
+int main() {
+
+    return 0;
+}
+
